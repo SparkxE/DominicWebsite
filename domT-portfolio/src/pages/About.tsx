51b96ccb7f375439b8cabc6dbec6871df2fda04f
Aaron Anderson <sylverblade16@gmail.com>
2026-01-09 15:14:42 -0600

    Started working on additional pages; added CSS file for additional pages

---
+++ domT-portfolio/src/pages/About.tsx
@@ -0,0 +1,13 @@
+import './Pages.css';
+function About(){
+    return(
+        <div id='about'>
+            <h1>About</h1>
+            <p>
+
+            </p>
+        </div>
+    )
+}
+
+export default About;
